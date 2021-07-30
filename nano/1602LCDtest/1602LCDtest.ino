b5fab9ac5e4707c93ba9ded1485fe5e57f9dbee7
Paul Bickerdyke <pbickerdyke@gmail.com>
2021-07-30 15:20:35 +0100

    testing i2c lcd

---
+++ nano/1602LCDtest/1602LCDtest.ino
@@ -0,0 +1,51 @@
+// Include the libraries:
+// LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C
+#include <Wire.h> // Library for I2C communication
+#include <LiquidCrystal_I2C.h> // Library for LCD
+
+// Wiring: SDA pin is connected to A4 and SCL pin to A5.
+// Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
+LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2); // Address , width, height
+
+// constants
+
+const byte NUM_CHARS = 16; // define width of display for use in loops
+
+// custom chars
+byte block[8] = {
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111
+};
+
+void progress() { // scroll custom char accross the screen like a progress bar
+  for (int i = 0; i <= NUM_CHARS-1; i++) {
+    lcd.setCursor(i,0); // move cursor using i as position
+    lcd.write((byte)0); // write the custom char
+    delay(200);
+  }
+}
+
+void setup() {
+  // Initiate the LCD:
+  lcd.init();
+  lcd.backlight();
+
+  lcd.createChar(0, block); // create a new custom character
+  progress();
+  delay(200);
+  lcd.clear();
+}
+
+
+void loop() {
+  lcd.setCursor(2, 0); // Set the cursor on the third column and first row.
+  lcd.print("Testing"); // Print the string 
+  lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
+  lcd.print("Writing!");
+}
